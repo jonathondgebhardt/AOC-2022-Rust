c4a9001ecaac3273bebaa16f9ae4e346086745eb
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2022-12-12 21:31:46 -0500

    day 11 part 1 complete.

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub enum Operation {
     Add,
@@ -6,23 +8,25 @@ pub enum Operation {
 
 #[derive(Debug)]
 pub struct Monkey {
-    items: Vec<u32>,
+    items: Vec<u128>,
     operation: Operation,
-    modifier: Option<u32>,
-    divisor: u32,
+    modifier: Option<u128>,
+    divisor: u128,
     true_monkey: usize,
     false_monkey: usize,
+    inspect_count: u32,
+    should_apply_worry: bool,
 }
 
 impl Monkey {
-    pub fn build(s: &[&str]) -> Self {
+    pub fn build(s: &[&str], apply_worry: bool) -> Self {
         let mut iter = s[1].split(": ");
         iter.next();
-        let items: Vec<u32> = iter
+        let items: Vec<u128> = iter
             .next()
             .unwrap()
             .split(", ")
-            .map(|n| n.parse::<u32>().unwrap())
+            .map(|n| n.parse::<u128>().unwrap())
             .collect();
 
         let operation = if s[2].contains("*") {
@@ -31,8 +35,13 @@ impl Monkey {
             Operation::Add
         };
 
-        // may be old instead of number
-        let modifier = match s[2].split_whitespace().rev().next().unwrap().parse::<u32>() {
+        let modifier = match s[2]
+            .split_whitespace()
+            .rev()
+            .next()
+            .unwrap()
+            .parse::<u128>()
+        {
             Ok(modifier) => Some(modifier),
             _ => None,
         };
@@ -42,7 +51,7 @@ impl Monkey {
             .rev()
             .next()
             .unwrap()
-            .parse::<u32>()
+            .parse::<u128>()
             .unwrap();
 
         let true_monkey = s[4]
@@ -68,27 +77,34 @@ impl Monkey {
             divisor,
             true_monkey,
             false_monkey,
+            inspect_count: 0,
+            should_apply_worry: apply_worry,
         }
     }
 
-    fn apply_worry(&self, item: u32) -> u32 {
-        match self.operation {
-            Operation::Add => match self.modifier {
-                Some(modifier) => item + modifier,
-                None => item + item,
-            },
-            Operation::Multiply => match self.modifier {
-                Some(modifier) => item * modifier,
-                None => item * item,
-            },
+    fn apply_worry(&self, item: u128) -> u128 {
+        if self.should_apply_worry {
+            match self.operation {
+                Operation::Add => match self.modifier {
+                    Some(modifier) => item + modifier,
+                    None => item + item,
+                },
+                Operation::Multiply => match self.modifier {
+                    Some(modifier) => item * modifier,
+                    // overflow on part 2
+                    None => item * item,
+                },
+            }
+        } else {
+            item
         }
     }
 
-    fn apply_bored(&self, item: u32) -> u32 {
+    fn apply_bored(&self, item: u128) -> u128 {
         item / 3
     }
 
-    fn get_monkey_receiver(&self, item: u32) -> usize {
+    fn get_monkey_receiver(&self, item: u128) -> usize {
         if item % self.divisor == 0 {
             self.true_monkey
         } else {
@@ -96,22 +112,20 @@ impl Monkey {
         }
     }
 
-    fn receive_items(&mut self, items: &mut Vec<u32>) {
+    fn receive_items(&mut self, items: &mut Vec<u128>) {
         self.items.append(items);
     }
 
-    fn receive_item(&mut self, item: u32) {
-        self.items.push(item);
+    pub fn remove_first(&mut self) -> u128 {
+        self.items.remove(0)
     }
 
-    pub fn drop_first(&mut self) {
-        self.items.remove(0);
-    }
-
-    pub fn inspect_item(&self) -> (u32, usize) {
+    pub fn inspect_item(&mut self) -> (u128, usize) {
         assert!(!self.is_done());
 
-        let mut item = self.items[0];
+        self.inspect_count += 1;
+
+        let mut item = self.remove_first();
         item = self.apply_worry(item);
         item = self.apply_bored(item);
 
@@ -121,26 +135,45 @@ impl Monkey {
     pub fn is_done(&self) -> bool {
         self.items.is_empty()
     }
+
+    pub fn get_inspect_count(&self) -> u32 {
+        self.inspect_count
+    }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let monkey_lines: Vec<&str> = input.lines().collect();
-    let mut monkeys: Vec<Monkey> = monkey_lines[..].chunks(7).map(Monkey::build).collect();
+pub fn get_monkey_business(s: &str, n: u32, apply_worry: bool) -> u128 {
+    let monkey_lines: Vec<&str> = s.lines().collect();
+    let mut monkeys: Vec<Monkey> = monkey_lines[..]
+        .chunks(7)
+        .map(|line| Monkey::build(line, apply_worry))
+        .collect();
 
-    for monkey in &monkeys {
-        while !monkey.is_done() {
-            let (item, receiver) = monkey.inspect_item();
-            println!("{} goes to {}", item, receiver);
+    for _ in 0..n {
+        for index in 0..monkeys.len() {
+            let monkey = &mut monkeys[index];
+            let mut sent_items: HashMap<usize, Vec<u128>> = HashMap::new();
 
-            let receiver = &mut monkeys[receiver];
+            while !monkey.is_done() {
+                let (item, receiver) = monkey.inspect_item();
+                sent_items.entry(receiver).or_default().push(item);
+            }
+
+            for (index, mut items) in sent_items {
+                monkeys[index].receive_items(&mut items);
+            }
         }
     }
 
-    None
+    monkeys.sort_by(|a, b| a.get_inspect_count().cmp(&b.get_inspect_count()).reverse());
+    monkeys[0].get_inspect_count() as u128 * monkeys[1].get_inspect_count() as u128
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u128> {
+    Some(get_monkey_business(input, 20, true))
+}
+
+pub fn part_two(input: &str) -> Option<u128> {
+    Some(get_monkey_business(input, 10_000, true))
 }
 
 fn main() {
@@ -163,7 +196,7 @@ mod tests {
         #[test]
         fn test_part_two() {
             let input = aoc::read_file("examples", 11);
-            assert_eq!(part_two(&input), None);
+            assert_eq!(part_two(&input), Some(2_713_310_158));
         }
     }
 }
